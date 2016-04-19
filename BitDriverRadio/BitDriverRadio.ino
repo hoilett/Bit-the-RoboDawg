aa896918f2ec10ca65ffe2f1288b690c637c7460
Orlando Hoilett <orlandohoilett@pal-nat186-6-245.itap.purdue.edu>
2016-04-19 09:52:25 -0400

    updated BitDriverRadio code to note which motor controls which wheel; updated ProjectPlan with a bit more description

--- BitDriverRadio/BitDriverRadio.ino
+++ BitDriverRadio/BitDriverRadio.ino
@@ -1,14 +1,27 @@
 #include <AFMotor.h>
 
-AF_DCMotor motor1(1);
-AF_DCMotor motor2(2);
-AF_DCMotor motor3(3);
-AF_DCMotor motor4(4);
+AF_DCMotor fright(1); //front right wheel is controlled by M1 on SainSmart motor driver shield
+AF_DCMotor fleft(2); //front left wheel is controlled by M2 on SainSmart motor driver shield
+AF_DCMotor bright(3); //back right wheel is controlled by M3 on SainSmart motor driver shield
+AF_DCMotor bleft(4); //back left wheel is controlled by M4 on SainSmart motor driver shield
 
 
 void setup()
 {
   Serial.begin(9600);
+
+  fright.setSpeed(255);
+  fright.run(FORWARD);
+  delay(2000);
+  fleft.setSpeed(255);
+  fleft.run(FORWARD);
+  delay(2000);
+  bright.setSpeed(255);
+  bright.run(FORWARD);
+  delay(2000);
+  bleft.setSpeed(255);
+  bleft.run(FORWARD);
+  delay(2000);
 }
 
 void loop()
