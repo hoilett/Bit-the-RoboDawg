6d8c569676d56bf680a12250e32c8a6f76faa44d
Orlando Hoilett <orlandohoilett@pal-nat186-6-245.itap.purdue.edu>
2016-04-19 09:32:08 -0400

    controlling robot chassis with nRF24L01 modules

---
+++ BitDriverRadio/BitDriverRadio.ino
@@ -0,0 +1,16 @@
+#include <AFMotor.h>
+
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+}
