c199f39cbd664dac93252ed33e0ffd76cbb1ad0c
Orlando Hoilett <orlandohoilett@pal-nat186-5-142.itap.purdue.edu>
2016-04-24 22:42:29 -0400

    added turning code

--- BitReceiver_withCMDCode_MotorDriving/BitReceiver_withCMDCode_MotorDriving.ino
+++ BitReceiver_withCMDCode_MotorDriving/BitReceiver_withCMDCode_MotorDriving.ino
@@ -24,6 +24,9 @@ int xDir;
 int yDir;
 int buttonVal;
 
+
+int zeroLine = 50;
+
 void setup(void)
 {
   //Serial.begin(115200);
@@ -91,40 +94,86 @@ void loop(void)
   }
 
 
+    if ((abs(xDir) - abs(yDir)) > zeroLine) //user is trying to go forward and backward
+    {
+      if(xDir > zeroLine)
+      {
+        analogWrite(l1_motor, xDir);
+        analogWrite(l2_motor, 0);
+        
+        analogWrite(r1_motor, xDir);
+        analogWrite(r2_motor, 0);
+      }
+      else if(xDir < -zeroLine)
+      {
+        analogWrite(l2_motor, abs(xDir));
+        analogWrite(l1_motor, abs(0));
+        
+        analogWrite(r2_motor, abs(xDir));
+        analogWrite(r1_motor, abs(0));
+      }
+      else 
+      {
+        analogWrite(l2_motor, 0);
+        analogWrite(l1_motor, abs(0));
+        
+        analogWrite(r1_motor, 0);
+        analogWrite(r2_motor, abs(0));      
+      }
+    }
+    else if
+    {
+      if(yDir > zeroLine) //turn right
+      {
+        analogWrite(l1_motor, 0);
+        analogWrite(l2_motor, 0);
+        
+        analogWrite(r1_motor, yDir);
+        analogWrite(r2_motor, 0);
+      }
+      else if(yDir < -zeroLine) //turn left
+      {
+        analogWrite(l2_motor, abs(yDir));
+        analogWrite(l1_motor, abs(0));
+        
+        analogWrite(r2_motor, 0);
+        analogWrite(r1_motor, 0);
+      }
+      else 
+      {
+        analogWrite(l2_motor, 0);
+        analogWrite(l1_motor, 0);
+        
+        analogWrite(r1_motor, 0);
+        analogWrite(r2_motor, 0);      
+      }
+    }
+    else
+    {
+      analogWrite(l2_motor, 0);
+      analogWrite(l1_motor, 0);
+        
+      analogWrite(r1_motor, 0);
+      analogWrite(r2_motor, 0); 
+    }
     
 
 
-    if(xDir > 50)
-    {
-      analogWrite(l1_motor, xDir);
-      analogWrite(l2_motor, 0);
-      
-      analogWrite(r1_motor, xDir);
-      analogWrite(r2_motor, 0);
-    }
-    else if(xDir < -50)
-    {
-      analogWrite(l2_motor, abs(xDir));
-      analogWrite(l1_motor, abs(0));
-      
-      analogWrite(r2_motor, abs(xDir));
-      analogWrite(r1_motor, abs(0));
-    }
-
-    else 
-    {
-      analogWrite(l2_motor, 0);
-      analogWrite(l1_motor, abs(0));
-      
-      analogWrite(r1_motor, 0);
-      analogWrite(r2_motor, abs(0));      
-    }
 
 //    //Serial.println(theMessage);
 //    Serial.print(buttonVal); Serial.print('\t');
 //    Serial.print(xDir); Serial.print('\t');
 //    Serial.println(yDir);
 
+
+
+
+
+
+
+
+
+    
 
     theMessage = "";
 
